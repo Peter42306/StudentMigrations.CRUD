ec545f856bc82957b71bf179ced738e5d19a9416
Petr Zalizko <pzalizko@gmail.com>
2024-10-16 18:42:21 +0300

    Add project files.

---
+++ StudentMigrations.CRUD/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace StudentMigrations.CRUD.Models
+{
+	public class ErrorViewModel
+	{
+		public string? RequestId { get; set; }
+
+		public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+	}
+}
